b0057d701ed8661c66b631fcd3e5f77dcb3eab14
Sam Steele <sam.steele@gmail.com>
2020-01-06 17:02:57 -0500

    fetch 2 pages of jumps to avoid gaps

--- edsm.py
+++ edsm.py
@@ -26,8 +26,10 @@ INFLUXDB_USERNAME = 'root'
 INFLUXDB_PASSWORD = 'root'
 INFLUXDB_DATABASE = 'edsm'
 points = []
+last = None
 
 def add_rank(data, activity):
+    global points
     points.append({
         "measurement": "rank",
         "time": date.today().isoformat() + "T00:00:00",
@@ -57,13 +59,51 @@ def distance(system1, system2):
     s1 = fetch_system(system1)
     s2 = fetch_system(system2)
 
-    dx = s1['coords']['x'] - s2['coords']['x']
-    dy = s1['coords']['y'] - s2['coords']['y']
-    dz = s1['coords']['z'] - s2['coords']['z']
+    dx = float(s1['coords']['x']) - float(s2['coords']['x'])
+    dy = float(s1['coords']['y']) - float(s2['coords']['y'])
+    dz = float(s1['coords']['z']) - float(s2['coords']['z'])
 
     return math.sqrt(dx*dx + dy*dy + dz*dz)
 
+def add_jump(src, dst):
+    global points
+    system = fetch_system(dst['system'])
+    if 'type' in system['primaryStar']:
+        points.append({
+            "measurement": "jump",
+            "time": datetime.fromisoformat(dst['date']).isoformat(),
+            "tags": {
+                "commander": EDSM_COMMANDER_NAME,
+                "system": dst['system'],
+                "firstDiscover": dst['firstDiscover'],
+                "primaryStarType": system['primaryStar']['type']
+            },
+            "fields": {
+                "distance": distance(src['system'], dst['system']),
+                "x": float(system['coords']['x']),
+                "y": float(system['coords']['y']),
+                "z": float(system['coords']['z'])
+            }
+        })
+    else:
+        points.append({
+            "measurement": "jump",
+            "time": datetime.fromisoformat(dst['date']).isoformat(),
+            "tags": {
+                "commander": EDSM_COMMANDER_NAME,
+                "system": dst['system'],
+                "firstDiscover": dst['firstDiscover']
+            },
+            "fields": {
+                "distance": distance(src['system'], dst['system']),
+                "x": float(system['coords']['x']),
+                "y": float(system['coords']['y']),
+                "z": float(system['coords']['z'])
+            }
+        })
+
 def fetch_jumps(time):
+    global last
     try:
         response = requests.get('https://www.edsm.net/api-logs-v1/get-logs',
             params={'commanderName':EDSM_COMMANDER_NAME, 'apiKey':EDSM_API_KEY, 'endDateTime':time})
@@ -75,23 +115,9 @@ def fetch_jumps(time):
     data = response.json()
     print("Got %s jumps from EDSM" % (len(data['logs'])))
 
-    last = None
     for jump in data['logs']:
-        system = fetch_system(jump['system'])
         if last != None:
-            points.append({
-                "measurement": "jump",
-                "time": datetime.fromisoformat(jump['date']).isoformat(),
-                "tags": {
-                    "commander": EDSM_COMMANDER_NAME,
-                    "firstDiscover": jump['firstDiscover'],
-                    "primaryStarType": system['primaryStar']['type']
-                },
-                "fields": {
-                    "system": jump['system'],
-                    "distance": distance(last['system'], jump['system'])
-                }
-            })
+            add_jump(jump, last)
         last = jump
 
     return data
@@ -146,6 +172,10 @@ add_rank(data, "Empire")
 
 requests_cache.install_cache('edsm')
 data = fetch_jumps(date.today().isoformat() + " 00:00:00")
+if len(data['logs']) > 0:
+    data = fetch_jumps(data['startDateTime'])
+    while len(data['logs']) == 0:
+        data = fetch_jumps(data['startDateTime'])
 
 try:
     client.write_points(points)
