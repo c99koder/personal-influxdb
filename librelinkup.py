967385962a76243f307c211933a0b71aae384336
Sam Steele <sam.steele@gmail.com>
2022-10-31 07:30:07 -0400

    Reuse authToken

--- librelinkup.py
+++ librelinkup.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import requests, sys
-from datetime import datetime, date, timedelta
+import requests, sys, os, json, time
+from datetime import datetime
 from config import *
 
 def append_reading(points, data, reading):
@@ -36,34 +36,49 @@ if not LIBRELINKUP_USERNAME:
     logging.error("LIBRELINKUP_USERNAME not set in config.py")
     sys.exit(1)
 
-STARTDATE = (date.today() - timedelta(days=date.today().weekday())).strftime("%Y-%m-%d %H:%M:%S")
 points = []
 
 connect(LIBRELINKUP_DATABASE)
 
-libreLinkUpHttpHeaders = {
+LIBRELINKUP_HEADERS = {
     "version": LIBRELINKUP_VERSION,
     "product": LIBRELINKUP_PRODUCT,
 }
 
+LIBRELINKUP_TOKEN = None
+script_dir = os.path.dirname(__file__)
+auth_token_path = os.path.join(script_dir, '.librelinkup-authtoken')
+if os.path.isfile(auth_token_path):
+    with open(auth_token_path) as json_file:
+            auth = json.load(json_file)
+            if auth['expires'] > time.time():
+                LIBRELINKUP_TOKEN = auth['token']
+                logging.info("Using cached authTicket, expiration: %s", datetime.fromtimestamp(auth['expires']).isoformat())
+
+if LIBRELINKUP_TOKEN is None:
+    logging.info("Auth ticket not found or expired, requesting a new one")
+    try:
+        response = requests.post(f'{LIBRELINKUP_URL}/llu/auth/login',
+            headers=LIBRELINKUP_HEADERS, json = {'email': LIBRELINKUP_USERNAME, 'password': LIBRELINKUP_PASSWORD})
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        logging.error("HTTP request failed: %s", err)
+        sys.exit(1)
+
+    data = response.json()
+    if not 'authTicket' in data['data']:
+        logging.error("Authentication failed")
+        sys.exit(1)
+
+    with open(auth_token_path, 'w') as outfile:
+        json.dump(data['data']['authTicket'], outfile)
+
+    LIBRELINKUP_TOKEN = data['data']['authTicket']['token']
+
+LIBRELINKUP_HEADERS['Authorization'] = 'Bearer ' + LIBRELINKUP_TOKEN
+
 try:
-    response = requests.post(f'{LIBRELINKUP_URL}/llu/auth/login',
-        headers=libreLinkUpHttpHeaders, json = {'email': LIBRELINKUP_USERNAME, 'password': LIBRELINKUP_PASSWORD})
-    response.raise_for_status()
-except requests.exceptions.HTTPError as err:
-    logging.error("HTTP request failed: %s", err)
-    sys.exit(1)
-
-data = response.json()
-if not 'authTicket' in data['data']:
-    logging.error("Authentication failed")
-    sys.exit(1)
-
-LIBRELINKUP_TOKEN = data['data']['authTicket']['token']
-libreLinkUpHttpHeaders['Authorization'] = 'Bearer ' + LIBRELINKUP_TOKEN
-
-try:
-    response = requests.get(f'{LIBRELINKUP_URL}/llu/connections', headers=libreLinkUpHttpHeaders)
+    response = requests.get(f'{LIBRELINKUP_URL}/llu/connections', headers=LIBRELINKUP_HEADERS)
     response.raise_for_status()
 except requests.exceptions.HTTPError as err:
     logging.error("HTTP request failed: %s", err)
@@ -77,7 +92,7 @@ if not 'data' in connections or len(connections['data']) < 1:
 logging.info("Using connection %s: %s %s", connections['data'][0]['patientId'], connections['data'][0]['firstName'], connections['data'][0]['lastName'])
 
 try:
-    response = requests.get(f'{LIBRELINKUP_URL}/llu/connections/{connections["data"][0]["patientId"]}/graph', headers=libreLinkUpHttpHeaders)
+    response = requests.get(f'{LIBRELINKUP_URL}/llu/connections/{connections["data"][0]["patientId"]}/graph', headers=LIBRELINKUP_HEADERS)
     response.raise_for_status()
 except requests.exceptions.HTTPError as err:
     logging.error("HTTP request failed: %s", err)
@@ -91,4 +106,3 @@ if len(data['data']['graphData']) > 0:
         append_reading(points, data, reading)
 
 write_points(points)
-
